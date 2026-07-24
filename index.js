5ad358d9aef236c4f3f546743d525655a74b0bc6
KING BM <killerbm8@gmail.com>
2026-07-24 20:48:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+app.use(express.static(__dirname));
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
